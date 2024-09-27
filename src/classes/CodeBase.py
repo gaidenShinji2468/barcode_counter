ba3a04e00929334952172fa259649fd827d4218c
gaidenshinji2468 <gaidenshinji2468@gmail.com>
2024-09-27 13:38:18 -0400

    BACKUP 202409271

---
+++ src/classes/CodeBase.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+
+
+class CodeBase:
+    def __init__(self):
+        self.code_base = pd.read_excel(f"{os.getcwd()}/data/code_base.xlsx").to_dict()
+
+    def get_brand(self, code):
+        return self.code_base["BRAND"][int(code)]
+
+    def get_piece(self, code):
+        for key, value in self.code_base.items():
+            if [
+                "SHIRTS_CODE"
+                "POLO_CODE"
+                "JACKET_CODE"
+                "FLANNEL_CODE"
+                "HOODIE_CODE"
+                "VARIOUS_CODE"
+                "LOWER_PARTS_CODE"
+            ].count(key) == 1:
+                for index, code128 in value.items():
+                    if int(code) == int(code128):
+                        return self.code_base[key[-len(key) : -(len(key) + 5)]][index]
--- src/main.py
+++ src/main.py
@@ -1,53 +1,21 @@
-import os
-import pandas as pd
-import keyboard
+from classes import CodeBase 
 
-scanned_codes = dict()
-# Variable temporal para capturar el código escaneado
-temp_code = ""
+def main():
+    scanned_codes = dict()
+    code_base = CodeBase()
 
-
-# Función que se ejecuta cuando se presiona Enter (fin de la lectura del código)
-def on_code_scanned(code):
-    global scanned_codes
-    # Limpiar los espacios innecesarios y agregar el código a la lista
-    code = code.strip()
-    if code:
+    print("Escanea los códigos de barras. Presiona f o F para salir.")
+    while True:
+        code = input()
         if scanned_codes.get(code):
             scanned_codes[code] += 1
         else:
             scanned_codes[code] = 1
-        print(f" Código escaneado: {code}")
-
-
-# Función para registrar las entradas del escáner
-def scan():
-    global temp_code
-    while True:
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_DOWN:
-            # Si es la tecla Enter, procesar el código escaneado
-            if event.name == "enter":
-                on_code_scanned(temp_code)
-                temp_code = ""  # Reiniciar la variable para el siguiente código
-            # Capturar el código escaneado
-            elif (
-                event.name != "enter" and len(event.name) == 1
-            ):  # Ignorar teclas especiales como Shift, etc.
-                temp_code += event.name
-
-        # Salir al presionar ESC
-        if event.name == "esc":
-            print("Salida.")
+        if code == "f" or code == "F":
             break
-
-
-def main():
-    print("Escanea los códigos de barras. Presiona ESC para salir.")
-    # Ejecutar la función de escaneo
-    scan()
-    # Mostrar los códigos escaneados
+    scanned_codes.popitem()
     print(f"\nCódigos escaneados: {sum(scanned_codes.values())}")
+    
     for code, count in scanned_codes.items():
         print(code, count, sep=" -> ")
 
